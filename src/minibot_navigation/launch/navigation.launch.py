46cb5b04775696fb2ad8f2b20ef2b7105a974c57
vihaan30g <vihaan30g@gmail.com>
2026-06-12 15:50:32 +0530

    Navigation part added (have errors)

---
+++ src/minibot_navigation/launch/navigation.launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
+
+def generate_launch_description():
+
+    params_file = os.path.join(
+        get_package_share_directory('minibot_navigation'),
+        'config',
+        'nav2_params.yaml'
+    )
+
+    return LaunchDescription([
+
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[params_file]
+        ),
+
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server',
+            output='screen',
+            parameters=[params_file]
+        ),
+
+        Node(
+            package='nav2_behaviors',
+            executable='behavior_server',
+            name='behavior_server',
+            output='screen',
+            parameters=[params_file]
+        ),
+
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=[params_file]
+        ),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_navigation',
+            output='screen',
+            parameters=[params_file]
+        ),
+
+    ])
